4ab73f47d3ebb998aa4bcea5b86c5571de7f6fe8
DavW <Davidpmwalsh@protonmail.com>
2020-01-17 12:52:18 +0000

    Spies

--- scripts/calc.js
+++ scripts/calc.js
@@ -3,6 +3,6 @@ function addition(numOne, numTwo) {
     if (typeof(numOne) == "number" && typeof(numTwo) == "number") {
         return numOne + numTwo;
     } else {
-        return "Error!";
+        alert("Error!");
     }
 }
